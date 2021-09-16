c7e1f158641cea86fed4ddf196e818f3cdcf9a58
vnguy4 <vanthoai.nguyen@aexp.com>
2021-09-16 11:49:51 +0100

    fixed typo

--- education/src/org/van/Greeting.java
+++ education/src/org/van/Greeting.java
@@ -4,6 +4,6 @@ public class Greeting {
     public static void main(String[] args) {
         String name = "Van";
         String game = "League";
-        System.out.println("Hello, my name " + name + " and I like " + game);
+        System.out.println("Hello, my name is " + name + " and I like " + game);
     }
 }
